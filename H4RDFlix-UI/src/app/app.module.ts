5c69a7b35aeb351a302750077e0048e3c637d900
Mateus Moraes <mtu_h@hotmail.com>
2021-11-28 17:31:55 -0300

    Material Carousel implementado
    ref: https://github.com/gabrielbusarello/material2-carousel

--- H4RDFlix-UI/src/app/app.module.ts
+++ H4RDFlix-UI/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCarouselModule } from '@ngbmodule/material-carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +17,7 @@ import { ShowcaseComponent } from './componentes/showcase/showcase.component';
     ShowcaseComponent
   ],
   imports: [
+    MatCarouselModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
