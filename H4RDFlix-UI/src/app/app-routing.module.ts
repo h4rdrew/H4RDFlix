48b90e84bc2804ea65f88f5ac649349ca9bf4926
Mateus Moraes <mtu_h@hotmail.com>
2021-11-28 14:50:35 -0300

    criado componente home page

--- H4RDFlix-UI/src/app/app-routing.module.ts
+++ H4RDFlix-UI/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { MainMenuComponent } from './componentes/main-menu/main-menu.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: MainMenuComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- H4RDFlix-UI/src/app/app.component.ts
+++ H4RDFlix-UI/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'H4RDFlix-UI';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.navigateByUrl('home');
+  }
 }
