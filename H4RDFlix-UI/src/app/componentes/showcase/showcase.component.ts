7206cf5698c8c4d41ca8ba20b1241b2f45477764
Mateus Moraes <mtu_h@hotmail.com>
2021-12-31 22:35:00 -0300

    referencia + exemplo

--- H4RDFlix-UI/src/app/componentes/showcase/showcase.component.ts
+++ H4RDFlix-UI/src/app/componentes/showcase/showcase.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-showcase',
   templateUrl: './showcase.component.html',
-  styleUrls: ['./showcase.component.scss']
+  styleUrls: ['./showcase.component.scss'],
 })
 export class ShowcaseComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSwiper(swiper: any) {
+    console.log(swiper);
   }
-
+  onSlideChange() {
+    console.log('slide change');
+  }
 }
