015fd54fd193c962353e829c0dc74e10dc1284ff
Mateus Moraes <mtu_h@hotmail.com>
2022-02-09 00:08:26 -0300

    DB Test

---
+++ H4RDFlix.API/DB/Models/Teste.cs
@@ -0,0 +1,11 @@
+﻿using Simple.DatabaseWrapper.Attributes;
+
+namespace H4RDFlix.API.DB.Models
+{
+    public class Teste
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ H4RDFlix.API/Services/ConnectionDB.cs
@@ -0,0 +1,34 @@
+﻿using H4RDFlix.API.DB.Models;
+using Simple.Sqlite;
+using Simple.Sqlite.Extension;
+using System.Reflection;
+
+namespace H4RDFlix.API.Services
+{
+    public class ConnectionDB
+    {
+        public ConnectionDB(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var rootPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var dbFile = Configuration.GetConnectionString("Default");
+            services.AddSingleton(ConnectionFactory.FromFile(Path.Combine(rootPath, dbFile)));
+
+            services.AddControllers()
+                    .AddNewtonsoftJson();
+        }
+        private void setupDatabase(IServiceProvider applicationServices)
+        {
+            var fac = applicationServices.GetService<ConnectionFactory>();
+            fac.GetConnection()
+              .CreateTables()
+              .Add<Teste>()
+              .Commit();
+        }
+    }
+}
